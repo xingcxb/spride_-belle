197b42139d7b08629d51e271d2e86e8782b462ab
symbol <xingcxb@hotmail.com>
2017-05-09 17:56:58 +0800

    增加图片抓取 增加图片保存

--- belle.py
+++ belle.py
@@ -3,22 +3,49 @@
 import requests
 import json
 import time
+import os
 from bs4 import BeautifulSoup
 
 class belle():
 	
 	##先获得图片集合入口，然后单页读取图片
 	def getpic_url(self):
-		url = "http://www.beautylegmm.com"
+		list_belle = []
+		main_url = "http://www.beautylegmm.com"
 		headers = {'user-agent' : 'easymbol-app/0.1'}
-		html = requests.get(url,headers = headers)
+		html = requests.get(main_url,headers = headers)
 
 		print("retnrn code = " + str(html.status_code))
-		#print(html.text)
+		
+		#获得首页图集
+		#暂不支持翻页效果
 		htmlmain = BeautifulSoup(html.text,"html.parser")
-		list_belle_url = htmlmain.find_all(class_='post_weidaopic')
-		for belle_url in list_belle_url:
-			print(belle_url.a['href'])
+		belle_url = htmlmain.find_all(class_='post_weidaopic')
+		for bellepage_url in belle_url:
+			list_belle.append(bellepage_url.a['href'])
+		
+		#抓取，图集下面的数据
+		#暂不支持翻页
+		i = 0
+		for url in list_belle:
+			pic_html = requests.get(url,headers = headers)
+			pic_info_html = BeautifulSoup(pic_html.text,"html.parser")
+			picInfo_url = "http://www.beautylegmm.com"
+			pic_list_url = pic_info_html.find_all('img')
+			for pic_url in pic_list_url:
+				print(pic_url)
+				newurl = pic_url['src']
+				if 'page' not in newurl:
+					newurl = picInfo_url + newurl
+					path_local = '/Users/symbol/Pictures/test'
+					data = requests.get(newurl)
+					fp = open(path_local + "/" + str(i) + ".jpg",'wb')
+					fp.write(data.content)
+					fp.close()
+					i+=1
+
+			print(pic_html.text)
+			break
 
 
 if __name__ == "__main__":
